5160cb1e4dee31816dc41d18a80439b2593c7e3c
valerio <valeriog@valerios-imac.home>
2023-09-03 23:06:46 +0100

    Started ch4 - statistics

---
+++ ch4/matrices.py
@@ -0,0 +1,84 @@
+from typing import List,Tuple
+from typing import Callable
+
+Matrix = List[List[float]]
+Vector = List[float]
+
+A = [[1, 2, 3],  # A has 2 rows and 3 columns
+     [4, 5, 6]]
+
+B = [[1, 2],     # B has 3 rows and 2 columns
+     [3, 4],
+     [5, 6]]
+
+d
+def shape(A: Matrix) -> Tuple[int, int]:
+    """Returns (# of rows of A, # of columns of A)"""
+    num_rows = len(A)
+    num_cols = len(A[0]) if A else 0   # number of elements in first row
+    return num_rows, num_cols
+
+assert shape([[1, 2, 3], [4, 5, 6]]) == (2, 3)  # 2 rows, 3 columns
+
+def get_row(A: Matrix, i: int) -> Vector:
+    """Returns the i-th row of A (as a Vector)"""
+    return A[i]             # A[i] is already the ith row
+
+def get_column(A: Matrix, j: int) -> Vector:
+    """Returns the j-th column of A (as a Vector)"""
+    return [A_i[j]          # jth element of row A_i
+            for A_i in A]   # for each row A_i
+
+
+
+def make_matrix(num_rows: int,
+                num_cols: int,
+                entry_fn: Callable[[int, int], float]) -> Matrix:
+    """
+    Returns a num_rows x num_cols matrix
+    whose (i,j)-th entry is entry_fn(i, j)
+    """
+    return [[entry_fn(i, j)             # given i, create a list
+             for j in range(num_cols)]  #   [entry_fn(i, 0), ... ]
+            for i in range(num_rows)]   # create one list for each i
+
+def identity_matrix(n: int) -> Matrix:
+    """Returns the n x n identity matrix"""
+    return make_matrix(n, n, lambda i, j: 1 if i == j else 0)
+
+assert identity_matrix(5) == [[1, 0, 0, 0, 0],
+                              [0, 1, 0, 0, 0],
+                              [0, 0, 1, 0, 0],
+                              [0, 0, 0, 1, 0],
+                              [0, 0, 0, 0, 1]]
+
+#
+# from this:
+friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
+               (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
+
+# we can get a matrix where we have 1 when there is a relationship between the users
+#            user 0  1  2  3  4  5  6  7  8  9
+#
+friend_matrix = [[0, 1, 1, 0, 0, 0, 0, 0, 0, 0],  # user 0
+                 [1, 0, 1, 1, 0, 0, 0, 0, 0, 0],  # user 1
+                 [1, 1, 0, 1, 0, 0, 0, 0, 0, 0],  # user 2
+                 [0, 1, 1, 0, 1, 0, 0, 0, 0, 0],  # user 3
+                 [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],  # user 4
+                 [0, 0, 0, 0, 1, 0, 1, 1, 0, 0],  # user 5
+                 [0, 0, 0, 0, 0, 1, 0, 0, 1, 0],  # user 6
+                 [0, 0, 0, 0, 0, 1, 0, 0, 1, 0],  # user 7
+                 [0, 0, 0, 0, 0, 0, 1, 1, 0, 1],  # user 8
+                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]]  # user 9
+# while it can be an inefficient representation if there are few connections
+# it makes searches very quick; e.g. to identify if 2, i,j people are friends or not, 
+# you just need to check their intersection [i,j]
+assert friend_matrix[0][2] == 1, "0 and 2 are friends"
+assert friend_matrix[0][8] == 0, "0 and 8 are not friends"
+
+# only need to look at one row to see who is friend of a particular person
+def friends_of_user(A:Matrix, i: int)->List:
+    return [  j                         # return the id of the user
+              for j in range(len(A[i])) # for every friendship relationship of user i
+              if A[i][j] == 1           # if they are friend
+           ]
