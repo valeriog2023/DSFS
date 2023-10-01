cc28e27015bd134a7bfee85e8f32c0ce37e383e5
valerio <valeriog@valerios-imac.home>
2023-10-01 23:11:05 +0100

    adding comments

--- ch6/probability2.py
+++ ch6/probability2.py
@@ -6,12 +6,15 @@ random.seed(0)
 def uniform_pdf(x:float) -> float:
     '''probability of a value in uniform distribution is 1/N where N is the events space
        if N is infinite the probability is zero for any specific event
+       Total area under the curve is one
     '''
     return 1 if 0 <= x < 1 else 0
 
 
 def uniform_cdf(x:float) -> float:
-    '''Returns the probability that a uniform random variable is <= x'''
+    '''Returns the probability that a uniform random variable is <= x
+       Total area under the curve is one
+    '''
     if x < 0: return 0    # probability is defined ony between 0 and 1
     elif x < 1: return x  # probability that X < x is x
     else: return 1        # probability max value is 
