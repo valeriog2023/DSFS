dd44229fd7d136a82bd7d3b3d3b129ca65fec525
valerio <valeriog@valerios-imac.home>
2023-10-01 23:09:25 +0100

    adding pdf file

---
+++ ch6/probability2.py
@@ -0,0 +1,26 @@
+import random, numpy as np
+from matplotlib import pyplot as plt
+
+random.seed(0)
+
+def uniform_pdf(x:float) -> float:
+    '''probability of a value in uniform distribution is 1/N where N is the events space
+       if N is infinite the probability is zero for any specific event
+    '''
+    return 1 if 0 <= x < 1 else 0
+
+
+def uniform_cdf(x:float) -> float:
+    '''Returns the probability that a uniform random variable is <= x'''
+    if x < 0: return 0    # probability is defined ony between 0 and 1
+    elif x < 1: return x  # probability that X < x is x
+    else: return 1        # probability max value is 
+
+points = np.arange(-1.0 ,2.0,0.01)
+values = [ uniform_pdf(float(x)) for x in points ]
+
+plt.plot(points,values)    # green solid line
+plt.title("Uniform Distribution - PDF")
+plt.show()
+
+
