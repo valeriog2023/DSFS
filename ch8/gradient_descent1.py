7e84db5cef0f5fa4b9cdef134fee62784e310046
valeriog_linuxhome <valerio.galantini2@gmail.com>
2023-10-15 12:25:51 +0100

    Adding test on gradient descent

--- ch8/gradient_descent1.py
+++ ch8/gradient_descent1.py
@@ -1,12 +1,78 @@
+from typing import Callable
 import sys
 sys.path.append("/home/vale6811/Desktop/oreilly/DSFS/ch4/")
-import vector,matrices
+import vector
+from matplotlib import pyplot as plt
+
 
 def sum_of_squares(v: vector.Vector) -> float:
     """Givena vector v returns the  sum of squares of the elements in the Vector using the vector dot product"""
     return vector.dot(v,v)
 
-print("Basic assert")
+print("Basic assert on sum of squares")
 assert sum_of_squares([2,2]) == 8
 assert sum_of_squares([2,2,0,2]) == 12
 print("Basic assert done")
+
+def difference_quotient(f: Callable[[float], float], x:float, h:float) -> float:
+    """
+    This method takes as input a:
+      f: a function that has as input a float and returns a float
+      x: a float that is used as the input of function x
+      h: a float that is used as a step
+
+    The method returns (f(x+h) - (fx)) / h
+    Note: for h->0 this is the definition of derivative
+    """
+    return (f(x+h) - f(x)) / h
+
+
+def square(x: float) -> float:
+    """Square function used to test the difference_quotient function"""
+    return x * x
+
+
+def square_derivative(x: float) -> float:
+    """Derivative of the square function (used for testing)"""
+    return 2 * x
+
+
+def partial_difference_quotient(f: Callable[[vector.Vector], float],  # function that has multiple input floats and returns a float
+                                v: vector.Vector,                     # starting point of the function f
+                                i: int,                        # selects the coordinate for which we run the derivative
+                                h: float                       # step
+                                ) -> float:
+    """
+    Returns the partial difference quotient of f at the point v for the coordinate i
+    it uses h as a step
+    """
+    v_h = [v_j + ( 0 if j != i else h) for j,v_j in enumerate(v)]
+    return (f(v_h) - f(v)) / h
+
+
+def estimate_gradient(f: Callable[[vector.Vector], float],
+                      v: vector.Vector,               # starting point of the function f
+                      h: float = 0.0001,              # step  (default is 0.0001)
+                     ) -> vector.Vector:              # returns a vector
+    """
+    Returns the vector of partial derivates of the function f at the point v
+    it uses h as a step (default is 0.0001)
+    """
+    return [ partial_difference_quotient(f,v,i,h) for i in range(len(v)) ]
+
+
+
+
+
+
+if __name__ == "__main__":
+    xs =range(-10,11)
+    actuals = [square_derivative(x) for x in xs]
+    estimates = [difference_quotient(square, x, h=0.001) for x in xs ]
+
+    # plot to show that the result is basically the same
+    plt.title("Actual Derivates of x^2 vs approximation")
+    plt.plot(xs,actuals,'rx', label='Actual')   # red x dot
+    plt.plot(xs,estimates, 'b+', label='Estimate')
+    plt.legend(loc='best')
+    plt.show()
---
+++ ch8/gradient_descent2.py
@@ -0,0 +1,73 @@
+from gradient_descent1 import estimate_gradient
+import random
+from typing import Callable
+import sys
+sys.path.append("/home/vale6811/Desktop/oreilly/DSFS/ch4/")
+from vector import Vector,distance,scalar_multiply,add
+msg = """
+#
+# Here we test the gradient descent against the sum of sqaure function
+# Note that the sum of square function has an absolute minumum when
+# each element of v is zero
+"""
+def gradient_step(v: Vector, gradient: Vector, step_size: float) -> Vector:
+    """
+    This function taks as in input:
+       v: coordinate of a point in the space
+       gradient: vector of partial derivates of a function f (must be in the same dimension of v)
+       ste_size: size of the step to take from v in the gradient direction
+
+    it returns the coordinates of the point reached starting from v and moving step_size in the
+    gradient direction
+    """
+    #
+    # basic check
+    assert len(v) == len(gradient), f"initial point and gradient vectors must be of the size got instead v ({len(v)}, gradient ({len(gradient)})"
+    #
+    # return the new point
+    return add(v, scalar_multiply(step_size,gradient) )
+
+
+print("running basic gradient step assert")
+assert gradient_step([1,1,1], [1,1,1], 1) == [2,2,2]
+assert gradient_step([1,1,1], [1,0,1], 1) == [2,1,2]
+assert gradient_step([1,1,0], [1,0,1], 2) == [3,1,2]
+print("running basic gradient step assert - done")
+
+def sum_of_square_gradient(v: Vector) -> Vector:
+    """This function gets a Vector as in input and returns
+       The vector of the partial derivates for the function sum of squares
+       f(v) := sum [ (xi ^ 2) for xi in v ]
+       when we do the gradient of partial derivates, all terms are constant
+       except the i ew are considering so the
+       resulting gradient vector is [ 2*xi for xi in v ]
+    """
+    return [ 2*x for x in v]
+
+
+if __name__ == '__main__':
+    #
+    # create a random vector size 3
+    # the uniform function returns a random number between 2 values
+    # 2 extremes included
+    initial_v = [ random.uniform(-10,10) for i in range(3) ]
+    v = initial_v.copy()
+    #
+    # set a tolerance
+    tolerance = 0.0001
+    #
+    # walk the function
+    for epoch in range(1000):
+        # get the gradient in v
+        grad = sum_of_square_gradient(v)
+        # take a negative step in grad direction and recompute v
+        v = gradient_step(v, grad, -0.01)
+        print(epoch, v)
+        #
+        # stop if we are close enough
+        if distance(v, [0,0,0]) < tolerance: break
+
+
+    assert distance(v, [0,0,0]) < tolerance, f"the final distance after 1000 iterations should be zero with tolerance {tolerance} but it's {distance(v, [0,0,0])}"
+    print(f"got from {initial_v} close to [0,0,0] with tolerance {tolerance} and a step of -0.01 in {epoch} iterations")
+
